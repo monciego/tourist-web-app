f56bdecf8a4f081f96e45d59f12a773734b7534c
Jericho <jerichobantiquete@gmail.com>
2022-07-17 17:32:34 +0800

    removed unnecessary scripts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,19 +17,6 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
-
-
-    {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js">
-    </script>
-
-    <script type="text/javascript" src="https://momentjs.com/downloads/moment-with-locales.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/flatpickr@latest/dist/plugins/rangePlugin.js">
-    </script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
-    <script type="text/javascript" src="https://npmcdn.com/flatpickr/dist/l10n/de.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" />
-    <link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr/dist/themes/airbnb.css" /> --}}
-
 </head>
 
 <body class="relative font-sans antialiased">
