da966c9ddf4aaa55bc95bf363cdfa43ce4762857
Jericho <jerichobantiquete@gmail.com>
2022-08-22 16:16:19 +0800

    Different text in every role

--- resources/views/pages/homepage/index.blade.php
+++ resources/views/pages/homepage/index.blade.php
@@ -19,14 +19,22 @@
                 <div class="absolute bg-black/20 inset-0"></div>
                 <div class="absolute left-1/2 transform -translate-x-1/2 bottom-14 text-center">
                     <h2 class="text-white text-lg text-center mb-3 sm:text-3xl font-bold">
-                        @auth
+                        @if (Auth::user()->hasRole('user'))
                         Let's start your journey, <br>
                         {{ Auth::user()->name }}!
-                        @endauth
+                        @endif
 
                         @guest
                         Not sure where to go? Perfect.
                         @endguest
+
+                        @if (Auth::user()->hasRole('superadministrator'))
+                        Work less & travel more.
+                        @endif
+
+                        @if (Auth::user()->hasRole('owner'))
+                        Travel better with us.
+                        @endif
                     </h2>
                     <a href=""
                         class="text-primary inline-flex items-center gap-2 active:scale-[.95] text-sm px-4 py-1 sm:text-lg sm:px-7 sm:py-2 rounded-lg shadow-md bg-white hover:bg-gray-200 font-bold">
