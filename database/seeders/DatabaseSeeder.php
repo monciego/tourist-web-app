b9424d3d59602ae881578f10a47d6718365b9262
Jericho <jerichobantiquete@gmail.com>
2022-08-22 14:36:29 +0800

    created a default account for superadministrator

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,12 @@ class DatabaseSeeder extends Seeder
         $this->call(LaratrustSeeder::class);
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+      $user = \App\Models\User::factory()->create([
+            'name' => 'Office',
+            'email' => 'municipalityofdasol@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+         $user->attachRole('superadministrator');
     }
 }
