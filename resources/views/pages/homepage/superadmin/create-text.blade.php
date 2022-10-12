26a0731f5946f6ae1fd89928300b0743b4715194
Jericho <jerichobantiquete@gmail.com>
2022-10-12 12:10:38 +0800

    display old value in input if data is exists

--- resources/views/pages/homepage/superadmin/create-text.blade.php
+++ resources/views/pages/homepage/superadmin/create-text.blade.php
@@ -44,20 +44,6 @@
             </a>
         </div>
     </div>
-
-
-    {{-- @else
-    <button x-on:click="open = true"
-        class="flex items-center gap-2 justify-center active:scale-[.98] text-sm rounded text-right text-white bg-indigo-600 hover:bg-indigo-800 px-4 py-1.5">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-4 h-4">
-            <path stroke-linecap="round" stroke-linejoin="round"
-                d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-        </svg>
-        Edit Price
-    </button>
-    @endif --}}
-
     <div x-show="open" x-cloak x-on:click="open = false"
         class="bg-black/40 z-[1000] fixed top-0 bottom-0 right-0 left-0">
     </div>
@@ -78,15 +64,17 @@
             @csrf
             <input name="id" type="hidden" value="{{ Auth::user()->id }}">
             <div>
+                @foreach ($homepage_datas as $homepage_data)
                 <x-label for="homepage_tagline" :value="__('Tagline')" />
-                {{-- @if(empty($properties->properties_details->property_price)) --}}
+                @if(empty($homepage_data->homepage_tagline))
                 <x-input id="homepage_tagline" class="block mt-1 w-full" type="text" name="homepage_tagline" required
                     autofocus />
-                {{-- @else
-                <x-input id="property_price" class="block mt-1 w-full" type="number" name="property_price"
-                    :value="old('property_price', $properties->properties_details->property_price)" required
-                    autofocus />
-                @endif --}}
+                @else
+                <x-input id="homepage_tagline" class="block mt-1 w-full"
+                    :value="old('homepage_tagline', $homepage_data->homepage_tagline)" type="text"
+                    name="homepage_tagline" required autofocus />
+                @endif
+                @endforeach
 
             </div>
             <button
