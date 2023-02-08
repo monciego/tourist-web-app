d145069388d52196986dff67d0a83959079b01d2
Jericho <jerichobantiquete@gmail.com>
2023-02-08 14:58:59 +0800

    addd footer

--- resources/views/pages/homepage/index.blade.php
+++ resources/views/pages/homepage/index.blade.php
@@ -35,13 +35,33 @@
         </a>
     </div>
     @endif
-
-
     @include('pages.homepage.inspiration')
-
-
     @auth
-
     @endauth
-
+    <footer class="p-4 bg-white shadow md:px-6 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="#" class="flex items-center mb-4 sm:mb-0">
+                <x-application-logo class="block" />
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0">
+                <li>
+                    <a href="https://dasolpangasinan.gov.ph/" target="__blank"
+                        class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="{{ route('listing.index') }}" class="mr-4 hover:underline md:mr-6">Destinations</a>
+                </li>
+                <li>
+                    <a href="{{ route('emergency-hotline.index') }}" class="mr-4 hover:underline md:mr-6 ">Emergeny
+                        Hotlines</a>
+                </li>
+                <li>
+                    <a href="tel:099412415" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center">© Dasol. All Rights Reserved.
+        </span>
+    </footer>
 </x-app-layout>
