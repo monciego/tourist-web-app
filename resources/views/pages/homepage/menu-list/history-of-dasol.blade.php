2bf6c39797b1b10a4f96b6a9af821409f408305f
Jericho <jerichobantiquete@gmail.com>
2023-03-20 00:27:16 +0800

    feat: history of dasol

--- resources/views/pages/homepage/menu-list/history-of-dasol.blade.php
+++ resources/views/pages/homepage/menu-list/history-of-dasol.blade.php
@@ -8,6 +8,104 @@
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">History of Dasol</p>
             </div>
 
+            <div class="tracking-wide mt-4">
+                <p class="indent-10 mb-2">
+                    In the early part of the 18th century, Dasol was just a part of the settlement, which today
+                    comprises
+                    the
+                    areas of the
+                    municipalities of Mabini, Burgos and Dasol. Those places had no fixed boundaries then as they were
+                    frontier settlements
+                    covered with thick forests and coastal fishing grounds that teemed with various fish species.
+                    Together
+                    with eight (8)
+                    other towns of Pangasinan, the place was within the territorial jurisdiction of Zambales province.
+                </p>
+                <p class="indent-10 mb-2">
+                    The town got its name from the medicinal herb “dosol” which abounded in the locality during the
+                    Spanish period. The
+                    leaves of this plant were greenish and oval in shape. It was a tuber relatively very low in height.
+                    It was commonly used
+                    to cure infected wounds, but was found to have the unusual characteristic of miraculously
+                    disappearing (abandoning its
+                    post) for unknown reasons, never to be located again. its name was frequently mispronounced as DASOL
+                    by the Spaniards,
+                    so that when the place was established as a municipality in the 19th century, DASOL became its
+                    official name.
+                </p>
+                <p class="indent-10 mb-2">
+                    This work is written especially to trace the leaders of our municipality from the Spanish Regime to
+                    the present time.
+                    Thus, many portions are taken from several sources of records.
+                </p>
+                <p class="indent-10 mb-2">
+                    In an article found at the Library of Colegio de San Sebastian, it was stated that the Recoletos
+                    friars were the ones
+                    who built the Roman Catholic Church in the area. In the history written by theses friars, the
+                    municipality was civilly
+                    constituted in the year 1793 and the mayors are hereby listed below with the corresponding years the
+                    held their offices.
+                </p>
+
+                <div class="text-center mt-4">
+
+                    <p class="mb-2">Don Antonio de la Cruz - 1793</p>
+                    <p class="mb-2">
+                        Don Juan de la Cruz - 1794
+                    </p>
+                    <p class="mb-2">
+                        Don Andres de Agustin - 1795
+                    </p>
+                    <p class="mb-2">
+                        Don Policarpio Vidal - 1796
+                    </p>
+                    <p class="mb-2">
+                        Don Francisco Espiritu - 1797
+                    </p>
+                    <p class="mb-2">
+                        Don Francisco Masaltao - 1798
+                    </p>
+                    <p class="mb-2">
+                        Don Domingo Mutana - 1799
+                    </p>
+                    <p class="mb-2">
+                        Don Nicolas Calixto - 1800
+                    </p>
+                    <p class="mb-2">
+                        Don Lorenzo Berzosa - 1801
+                    </p>
+                    <p class="mb-2">
+                        Don Eduardo de Gula - 1802
+                    </p>
+                    <p class="mb-2">
+                        Don Diego Manuel - 1803
+                    </p>
+                    <p class="mb-2">
+                        Don Cipriano de Castro - 1804
+                    </p>
+                    <p class="mb-2">
+                        Don Juan de los Reyes - 1805
+                    </p>
+                    <p class="mb-2">
+                        Don Juan de Cano - 1806
+                    </p>
+                    <p class="mb-2">
+                        Don Mateo de la Cruz - 1807
+                    </p>
+                    <p class="mb-2">
+                        Don Lorenzo Berzosa - 1808
+                    </p>
+                    <p class="mb-2">
+                        Don Ygnacio Encarnacion - 1809
+                    </p>
+                    <p class="mb-2">
+                        Don Lorenzon Berzosa - 1810
+                    </p>
+
+                </div>
+            </div>
+
+
         </div>
     </div>
 </x-app-layout>
