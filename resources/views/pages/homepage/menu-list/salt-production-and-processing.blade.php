2a9e19da7cf59d2c51957205a776e18d32feacc6
Jericho <jerichobantiquete@gmail.com>
2023-03-20 07:45:21 +0800

    feat: salt production and processing

--- resources/views/pages/homepage/menu-list/salt-production-and-processing.blade.php
+++ resources/views/pages/homepage/menu-list/salt-production-and-processing.blade.php
@@ -9,6 +9,44 @@
                     Processing</p>
             </div>
 
+            <div class="text-wide my-4">
+                <p class="mb-2">
+                    There are methods to produce salt, the solar evaporation being the oldest method, its use a
+                    practical only in warm
+                    climates where the evaporation rate exceeds the participation rate, either annually or extended
+                    periods.
+                </p>
+                <p class="mb-2">
+                    Solar salt production is, typically, the capturing of sea salt water in shallow ponds where the sun
+                    evaporates most of
+                    the water. The concentrated brine precipitates the salt which is then gathered by the Dasolano salt
+                    makers through the
+                    process called kayod.
+                </p>
+                <p class="mb-2">
+                    Usually, two types of ponds are used, first us the concentrated pond where the sea water from the
+                    ocean or salt lake is
+                    stored. The second is called the crystallizing pond or banigan, where the salt is actually produced.
+                    The capturing of
+                    salt water in shallow ponds or banigan where the sun evaporates most of the water.
+                </p>
+                <p class="mb-2">
+                    By noon, the salt beds turn white, indicating that salt crystal have been formed. Before the sunset,
+                    the farmworker,
+                    using a rake called kayod collects the crystal kamarin, then salt is packed in sacks ready to be
+                    collected by buyers.
+                </p>
+                <p class="mb-2">
+                    Salt makers start cleaning the tiled beds at the start of the salt making season in October and
+                    harvest salt from
+                    December to May before the start of rainy season.
+                </p>
+
+                <div class="mt-4">
+                    <img src="{{asset('assets/images/dasol-product/asin.png') }}" alt="" class="h-[25rem]">
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
