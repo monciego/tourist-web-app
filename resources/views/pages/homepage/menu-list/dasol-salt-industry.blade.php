a435d56a88a40ebf97982d47fcf684601aa53f8b
Jericho <jerichobantiquete@gmail.com>
2023-03-20 07:54:28 +0800

    feat: dasol salt industry

--- resources/views/pages/homepage/menu-list/dasol-salt-industry.blade.php
+++ resources/views/pages/homepage/menu-list/dasol-salt-industry.blade.php
@@ -8,6 +8,34 @@
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Dasol Salt Industry</p>
             </div>
 
+            <div class="text-wide my-4">
+                <p class="mb-2">
+                    In a province on the northwest part of Luzon Island, there lies a town that lives on salt—Dasol.
+                    Salt-making has been
+                    the way of life since time could remember. According to data, Dasol is the only municipality in the
+                    first district of
+                    Pangasinan that has been using salt beds or banigans and the traditional method of solar salt
+                    making. This is the oldest
+                    method of salt production.
+                </p>
+                <p class="mb-2">
+                    Out of the eighteen (18) barangays in the Municipality of Dasol, eight (8) of which engaged in
+                    commercial salt
+                    production, barangay Amalbalan, Bobonot, Gais-Guipe, Hermosa, Magsaysay, Malacapas, Poblacion and
+                    Uli.
+                </p>
+                <p class="mb-2">
+                    Salt production start by December until May, peaks during the summer months with estimated total of
+                    nine thousand four
+                    hundred ninety four (9,494) salt beds.
+                </p>
+
+
+                <div class="mt-4">
+                    <img src="{{asset('assets/images/dasol-product/asinan.png') }}" alt="" class="h-[25rem]">
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
