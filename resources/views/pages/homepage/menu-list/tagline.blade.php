18c0595ee1c43df390fe08bbd67f5cea1206572f
Jericho <jerichobantiquete@gmail.com>
2023-03-20 00:05:22 +0800

    feat: tagline

--- resources/views/pages/homepage/menu-list/tagline.blade.php
+++ resources/views/pages/homepage/menu-list/tagline.blade.php
@@ -8,6 +8,119 @@
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Tagline</p>
             </div>
 
+            <p class="my-4 text-center">(SB Ordinance No. 6, S. 2014)
+                (Approving the Official Tagline)</p>
+
+
+            <div class="tracking-wide">
+                <p class="font-medium mb-2">
+                    Why Dazzling Dasol?
+                </p>
+                <p class="indent-10 mb-2">
+                    Tagline is a brand name of municipality. For a municipality to be recognized and become favorite
+                    from
+                    all points of the
+                    globe, it must have a brand name or a trademark that will represent its totality; from nature,
+                    resources, culture and
+                    traditions.
+                </p>
+                <p class="indent-10 mb-2">
+                    Dasol is endowed with natural beauty, the acclaimed “Home of Quality Salt”. It has the most natural,
+                    purest and whitest
+                    salt in the vicinity, clear and crystal blue waters of Dasol Bay, long and cleanest rivers, rich
+                    mangroves, green hills
+                    and mountains overlooking the vast West Philippine Sea, including sparkling smiles of the joyful
+                    Dasolinians.
+                    Dazzling means impressive, owing, something bright, glaring, glittering, intense and overwhelming.
+                </p>
+                <p class="indent-10 mb-2">
+                    As we are all aware of, Dasol has an awe inspiring beauty and resources locally and internationally
+                    known, thus, by
+                    association and description, we have a “DAZZLING DASOL”.
+                </p>
+
+                <img class="my-4" src="{{ asset('assets/images/about/dazzling-dasol.png') }}" alt="Dazzling Dasol">
+
+                <p class="mb-2 font-medium">
+                    Description of the Symbol:
+                </p>
+
+                <p class="mb-2">
+                    Dasol stands in symbolic manner (please refer to symbol) where:
+                </p>
+                <p class="mb-2">
+                    “D” symbolizes a big pan to catch all the best and dreams for Dasol.
+                </p>
+                <p class="mb-2">
+                    “A” means balancing pillars representing stakeholders; in order that the vision for Dasol will be
+                    achieved, a
+                    private-public partnership is vital – combined with benchmarks/parameters to meet the highest ideals
+                    of development.
+                </p>
+                <p class="mb-2">
+                    “S” stands for balancing plunk in order that the visions, gifts and resources of Dasol will not be
+                    thrown away and will
+                    always be in equilibrium and sustainable.
+                </p>
+                <p class="mb-2">
+                    “O” represents a watch or a timer; that all actions taken to attract and meet development has its
+                    perfect timing and it
+                    must be now.
+                </p>
+                <p class="mb-2">
+                    “L” depicts the time three o’clock; the holy hour and the salt harvest time. The salt is an iconic
+                    product of Dasol, the
+                    most natural, purest and whitest, thus, associating Dasol into a dazzling one.
+                </p>
+
+                <p class="mb-2 font-medium">
+                    Other explanations and descriptions:
+                </p>
+
+                <ul>
+                    <li class="list-disc list-inside">The orange color of the emblem is between red and yellow that is
+                        like the
+                        color of fire and
+                        carrots; fire, representing
+                        burning passion towards development and carrots meaning a reward or advantage. Dasol is now in
+                        the State of receiving
+                        several awards because of from Exemplary Achievements.
+                    </li>
+                    <li class="list-disc list-inside">The Blue color stands for calmness and determination as
+                        Dasolinians are peace
+                        loving people but
+                        determined in the edge
+                        of time
+                    </li>
+                    <li class="list-disc list-inside">
+                        The black color represents that all actions of development are time bounded.
+                    </li>
+                    <li class="list-disc list-inside">
+                        The red one implies alertness and readiness of the stakeholders and managers of development
+                        meeting the need of every
+                        Dasolinian.
+                    </li>
+                    <li class="list-disc list-inside">The yellowish color surrounding the sign represents glaring light
+                        putting
+                        Dasol in all times in
+                        the memory of beholder.
+                    </li>
+                    <li class="list-disc list-inside">The picture inside show the iconic destinations in Dasol, as we
+                        know Tambobong
+                        is the famous
+                        white beach, that when
+                        Colibra Island is mentioned it must be in Tambobong, and when its Tambobong it is certainly
+                        Dasol, the municipal hall
+                        depicting the hub of governance, including the local products, these derived from the centennial
+                        logo.</li>
+                    <li class="list-disc list-inside">The color white in Dazzling shows purity and brightness standing
+                        for deep
+                        impressions for
+                        Dasol.</li>
+
+                </ul>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
