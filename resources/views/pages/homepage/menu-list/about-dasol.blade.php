96c9811a7349b7e0d8cd973352ce5dc9bea810b9
Jericho <jerichobantiquete@gmail.com>
2023-03-20 07:39:35 +0800

    feat: about dasol

--- resources/views/pages/homepage/menu-list/about-dasol.blade.php
+++ resources/views/pages/homepage/menu-list/about-dasol.blade.php
@@ -8,6 +8,135 @@
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">About Dasol</p>
             </div>
 
+            <div class="text-wide mt-8">
+                <p class="mt-4 font-bold text-center text-lg">
+                    Mandate
+                </p>
+
+                <p class="mt-2">
+                    The Office of the Mayor is where people are directed to or feel the need to go to when going to the
+                    Municipal Hall. It
+                    is an activity hub, almost daily it receives all mails, calls, inquiries, request, policy proposals,
+                    lobbyists, courtesy
+                    calls paperwork, etc.
+                </p>
+
+                <p class="mt-2">
+                    As the Local Chief Executive, he is expected to be visible in the locality inspecting on – going
+                    projects, holding
+                    public meetings, and consultation with the constituents, visiting the barangays, attending public
+                    functions among other
+                    things.
+                </p>
+
+                <p class="mt-2">
+                    He is the EO attending to the management and administrative matters in the office.
+                </p>
+
+                <p class="my-4 font-bold text-center text-lg">
+                    Vision
+                </p>
+
+                <p class="mt-2">
+                    Dasol is a world-class tourist destination, supportive to a sustainable agri-industrial economy and
+                    ecologically balance
+                    environment, with God-fearing, peace loving and united community.
+                </p>
+
+                <p class="my-4 font-bold text-center text-lg">
+                    Mission
+                </p>
+                <p class="mt-2">
+                    To realize the vision of Dasol, the people shall commit to do the following:
+                </p>
+                <ul>
+                    <li class="list-disc list-inside">
+                        Promote sustainable development and ecologically balanced environment.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Promote active public participation.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Install system procedures for an efficient and effective delivery of services.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Promote a well-balanced and a total human resource development.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Improved provision of basic services.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Improved social infrastructure facilities and tourism.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Strengthening of small-scale industries centered on agriculture.
+                    </li>
+                    <li class="list-disc list-inside">
+                        Promotion of sustainable development toward a safe, wholesome and greener environment.
+                    </li>
+                </ul>
+
+
+                <p class="my-4 font-bold underline text-lg">
+                    MUNICIPAL BOUNDARIES
+                </p>
+                <ul>
+                    <li class="list-disc list-inside">
+                        North – Burgos
+                    </li>
+                    <li class="list-disc list-inside">
+                        East – Mabini
+                    </li>
+                    <li class="list-disc list-inside">
+                        South – Infanta
+                    </li>
+                    <li class="list-disc list-inside">
+                        West – China Sea
+                    </li>
+                </ul>
+
+                <p class="mt-4">
+                    <span class="font-bold">DASOL</span> is a small town in Western Pangasinan, situated in a plateau.
+                    It is a third class municipality in the Province of
+                    Pangasinan, Philippines. It is composed and subdivided into Eighteen (18) Barangays and has a
+                    population of 29, 110
+                    people.
+                </p>
+                <p class="mt-2">
+                    Dasol is popular town for the production of industrial rock Salts. Dasol Bay occupies the whole
+                    coastline of Dasol and
+                    it is where the town gets its saltwater for its production. Dasol has an area of about 230 square
+                    kilometers.
+                </p>
+                <p class="mt-2">
+                    Dasol got its name from a medical herb called “DOSOL” which abound in the area. The Spaniards hardly
+                    pronounced “dosol”
+                    as when spoken, the word sounded as “DASOL”. It became its official name when it was created as
+                    municipality.
+                </p>
+                <p class="mt-2">
+                    On top of all its attractions, Dasol is famous for its Tambobong White Beach where the Colibra and
+                    Crocodile Islands are
+                    situated. Tambobong is far more peaceful and pristine.
+                </p>
+                <p class="mt-2">
+                    Our municipality is a place where glittering white salts are produces, which is one of the main
+                    livelihoods of our local
+                    folks, a traditional way of salt production is being practiced and aside from salt production which
+                    we can brag that our
+                    town is a “Home of Quality Salt” we can also be proud of our Caves, Beaches and Islands for foremost
+                    Tourist attractions
+                    where we can assure guests, tourists and investors that our town an offer “a home away from home”.
+                </p>
+
+                <div class="my-4">
+                    <img src="{{ asset('assets/images/about/discover-explore-love.png') }}" class="h-[25rem]"
+                        alt="Discover, explore and love Dasol… My Town, Your Town.">
+                </div>
+
+                <p class="mt-4">Discover, explore and love Dasol… My Town, Your Town.</p>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
