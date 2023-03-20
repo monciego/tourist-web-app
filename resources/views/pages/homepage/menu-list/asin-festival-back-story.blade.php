898ed771d7b357ec2a581ed7a71c570fc88a2a16
Jericho <jerichobantiquete@gmail.com>
2023-03-20 08:08:00 +0800

    feat: asin festival back story

--- resources/views/pages/homepage/menu-list/asin-festival-back-story.blade.php
+++ resources/views/pages/homepage/menu-list/asin-festival-back-story.blade.php
@@ -8,6 +8,77 @@
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Asin Festival Back Story</p>
             </div>
 
+            <div class="text-wide my-4">
+                <p class="mb-2">
+                    Salt is the main reason why the municipality walls are built, so this valuable treasure needs to be
+                    the center of
+                    attention in Dasol, Pangasinan.
+                </p>
+                <p class="mb-2">
+                    Salt Industry and Salt Making has been one of the main sources of livelihoods and income amongst the
+                    Dasolano families.
+                    It is also one of the major products that have brought the tagline “Dasol, Home of Quality Salt”.
+                </p>
+                <p class="mb-2">
+                    Back in 2008, a lady legislator then in her time Councilor Guendolin Estrada Criistobal-Bustamante,
+                    having the vision
+                    and dream of boasting her town, and letting the neighboring municipalities see the beauty and
+                    culture of Dasol, then
+                    launched the first ever Salt Activity in Marcelo Jimenez Memorial High School, now Don Marcelo
+                    Jimenez Memorial
+                    Polytechnic Institute. That the Asin Sculpture event provided platform for High School artists and
+                    contributors showcase
+                    their talent and ideas in promoting the beauty of salt sculpture and highlighted salt as the main
+                    component for the
+                    sculpture.
+                </p>
+                <p class="mb-2">
+                    The following year, 2009 another event was unveiled by Councilor Gwen Bustamante which featured
+                    “Cultural Presentation
+                    Show featuring Salt Making”.
+                </p>
+
+                <p class="mb-2">
+                    In the year 2015, the First Asin Festival, under the administration of of Mayor Noel A. Nacar was
+                    celebrated showcasing
+                    101 Uses of Salt, Saltscaping and Street Dancing and the groundbreaking of the Welcome Arch “Dasol,
+                    Home of Quality
+                    Salt”.
+                </p>
+
+                <p class="mb-2">
+                    In 2017, under the administration of Hon. Eric J. Versoza, an ordinance was enacted and authored by
+                    Hon. Nelson C.
+                    Espinosa making the Asin Festival official and permanent. Through this ordinance, it is the vision
+                    of the author that
+                    this Asin Festival becomes one of the best festivals in the Province of Pangasinan and at part with
+                    the well-knowm
+                    festivals of the country. In fact its name was derived from <span
+                        class="font-bold">“PANAGASINAN”</span> or where salt is made, PangASINan.
+                </p>
+
+                <p class="mb-2">
+                    Moreover, Dasol is one of the few towns in the province which contributes almost one-third (1/3) of
+                    our country’s salt
+                    production.
+                </p>
+
+                <p class="mb-2">
+                    Asin Festival celebrates the impact of people converging through creative practices in arts,
+                    innovation, history,
+                    culture and heritage providing platform for the Dasolanos to contribute, connect and promote the
+                    Salt product and the
+                    Municipality of Dasol as well.
+                </p>
+
+                <p class="mb-2">
+                    Eleven years after, the celebration of Dasol Asin Festival flourished and will continue to draw
+                    crowd enjoy and love the
+                    festivity.
+                </p>
+
+            </div>
+
         </div>
     </div>
 </x-app-layout>
