5a566008f457a0218936b5529133696dcb74c996
Jericho <jerichobantiquete@gmail.com>
2023-03-20 09:33:13 +0800

    removed 'municipality of dasol'

--- resources/views/pages/homepage/menu-list/miss-dasol-and-asin-festival-history.blade.php
+++ resources/views/pages/homepage/menu-list/miss-dasol-and-asin-festival-history.blade.php
@@ -4,7 +4,6 @@
     <div class="bg-white py-16 sm:py-20">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl lg:text-center">
-                <h2 class="text-lg font-semibold leading-8 tracking-tight text-indigo-600">Municipality of Dasol</h2>
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Miss Dasol and Asin Festival
                     History</p>
             </div>
