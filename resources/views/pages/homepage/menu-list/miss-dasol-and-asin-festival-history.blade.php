1030b37cb717be6f084866e84831f0a6c3ee0210
Jericho <jerichobantiquete@gmail.com>
2023-03-20 08:12:28 +0800

    feat: Miss Dasol and Asin Festival History

--- resources/views/pages/homepage/menu-list/miss-dasol-and-asin-festival-history.blade.php
+++ resources/views/pages/homepage/menu-list/miss-dasol-and-asin-festival-history.blade.php
@@ -9,6 +9,96 @@
                     History</p>
             </div>
 
+            <div class="text-wide my-4">
+                <p class="mb-2">
+                    In 2015, a Beauty Pageant to transform candidates to be responsible women for the community and as
+                    ambassadress of
+                    goodwill to promote Dasol nationally and internationally, was launched and this year was the birth
+                    of the Miss Dasol
+                    Asin Festival Queen.
+                </p>
+                <p class="mb-2">
+                    Spearheaded by the MDAF Chairwoman Madam Melcholy Balaoing Madayag, with Eight (8) beautiful and
+                    dazzling candidates
+                    from barangay Alilao, Amalbalan, Gais-Guipe, Hermosa, Magsaysay, Malacapas, Malimpin, Poblacion and
+                    Tambobong who
+                    equally represented their barangays with beauty, wit and talent.
+                </p>
+                <p class="mb-2">
+                    It was indeed the first modernized pageant in the municipality and the first Miss Dasol Asin
+                    Festival Queen 2015 was
+                    Miss Princess Daquioag of barangay Malimpin, Miss Everlyn Balazon of Gais-Guipe was the MDAF first
+                    runner up and Miss
+                    Jennifer Nacional of Barangay Tambobong bagged the Second Princess title.
+                </p>
+                <p class="mb-2">
+                    After a very successful launching of a modernized beauty pageant which is the MDAF 2015, years
+                    thereafter the continuity
+                    of the MDAF pageant was successfully fulfilled every year.
+                </p>
+                <p class="mb-2">
+                    In 2016 another set of equally and talented Dasolano women competed for the prestigious title Miss
+                    Dasol Asin Festival
+                    Queen 2016, chaired by Madam Donna Alqueza, with eleven (11) candidates representing different
+                    barangays, and Miss Julie
+                    Ann Rose Mojeca of Barangay Eguia was crowned MDAF Queen 2016.
+                </p>
+                <p class="mb-2">
+                    In 2017 another batch of lovely Dasolano ladies competed for the respected MDAF title, with a total
+                    of fourteen (14)
+                    lovely candidates from different barangays, and this year was headed by Hon. Edwin M. Morado. Miss
+                    Rizza Mae Colcol of
+                    Barangay Macalang was crowned as Miss Dasol Asin Festival 2017.
+                </p>
+                <p class="mb-2">
+                    A year afterward came 2018 and the legacy of Miss Dasol Asin Festival dawdles, with the new
+                    Chairperson and the
+                    permanent Executive Chairperson of Miss Dasol Asin Festival 2018 and all the following years
+                    thereafter, the Municipal
+                    Tourism and Cultural Affairs Officer Guendolin E. Cristobal – Bustamante, a new bunch of lovely
+                    talented and beautiful
+                    Dasolano women competed for the coveted MDAF title, fourteen (14) candidates from Barangays
+                    Amalbalan, Bobonot,
+                    Gais-Guipe, Hermosa, Eguia, Malacapas, Malimpin, Osmeña, Petal, Poblacion, Uli, Tambac, Tambobong
+                    East and Tambobong
+                    West vied for the MDAF 2018 crown. Miss Poblacion, Reggie Ann Corpuz was crowned Miss Dasol 2018.
+                </p>
+                <p class="mb-2">
+                    Four years after its birth, Miss Dasol Asin Festival has become Dasol’s renowned beauty pageant with
+                    a cause. Year after
+                    year, the transition, evolution and progression has defined and highlighted the importance of Beauty
+                    Pageant in the
+                    Municipality. Enjoined with the yearly ASIN FESTIVAL, MISS DASOL ASIN FESTIVAL PAGEANT is one of the
+                    highpoints of the
+                    said event followed by the Street Dancing Competition.
+                </p>
+                <p class="mb-2">
+                    This year, headed by the same Executive Chairperson Guendolin E. Cristobal – Bustamante, sixteen
+                    (16) gorgeous,
+                    stunning, talented and witty Dasolano ladies from Barangays Amalbalan, Bobonot, Eguia, Gais-Guipe,
+                    Hermosa, Macalang,
+                    Magsaysay, Malacapas, Malimpin, Osmeña, Petal, Poblacion, San Vicente, Tambac, Tambobong and Uli are
+                    prepped up to vie
+                    for the well known Miss Dasol Asin Festival Queen 2019.
+                </p>
+                <p class="mb-2">
+                    Three (3) crowns are at stake this year. The MDAF Queen 2019, MDAF Tourism & Environment and the
+                    MDAF Culture &
+                    Heritage.
+                </p>
+                <p class="mb-2">
+                    It is indeed a beauty pageant that marked Dasol’s pageant history and the legacy of which continues.
+                </p>
+                <a target="__blank" href="https://www.facebook.com/missdasolasinfestival" class="mb-2 text-indigo-600">
+                    Miss Dasol Facebook Page
+                </a>
+
+
+                <div class="mt-4">
+                    <img src="{{asset('assets/images/miss-dasol/miss-dasol.png') }}" alt="Miss Dasol" class="h-[25rem]">
+                </div>
+            </div>
+
         </div>
     </div>
 </x-app-layout>
