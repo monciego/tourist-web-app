774b7ac8fb142c1ef3fa4a255c059ab2a31625a1
Jericho <jerichobantiquete@gmail.com>
2023-03-23 21:52:15 +0800

    refactor: email verification message

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,12 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+            VerifyEmail::toMailUsing(function ($notifiable, $url) {
+        return (new MailMessage)
+            ->subject('Email Verification Request')
+            ->line('Thank you for registering with Tourist Application of Dasol. We are committed to protecting your privacy and ensuring the security of your account. To complete the registration process and verify your email address, we kindly request that you follow the instructions below:')
+            ->line('Click on the following link to below to activate your account:')
+            ->action('Verify Email Address', $url);
+    });
     }
 }
