07e20ffde044b6d2b33054fbadc1be32db908a8b
monciego <jerichobantiquete@gmail.com>
2023-04-23 21:36:09 +0800

    feat: most visited place

--- app/Http/Controllers/ReviewAndRatingController.php
+++ app/Http/Controllers/ReviewAndRatingController.php
@@ -4,26 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Properties;
 use App\Models\Review;
-use App\Models\TourRegistration;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ReviewAndRatingController extends Controller
 {
     public function index() {
-
-      /*   $most_visited = Properties::with('tour_registration')->has('reviews')->get();
-
-        dd($most_visited); */
+        $most_visited_places = DB::table('tour_registrations')
+            ->join('properties', 'tour_registrations.property_id','=','properties.id')
+            ->select('tour_registrations.property_id','properties.property_name',
+                DB::raw('SUM(number_of_adults) as total_number_of_adults', ),
+                DB::raw('SUM(number_of_children) as total_number_of_children'),
+                DB::raw('SUM(number_of_infants) as total_number_of_infants'),
+                DB::raw('SUM(number_of_foreigner) as total_number_of_foreigner'))
+            ->where('status', 'already_left')
+            ->groupBy('tour_registrations.property_id', 'properties.property_name')
+            ->orderBy(DB::raw("`number_of_adults` + `number_of_children` + `number_of_infants` + `number_of_foreigner`"), 'desc')
+            ->get();
 
         $properties = Properties::with('business_legal_documents', 'properties_details', 'frequently_questions', 'frequently_questions.frequently_answer', 'reviews')->has('reviews')->withCount('reviews')->orderBy('reviews_count', 'desc')->get();
-        return view('superadmin.review-and-rating.index', compact('properties'));
+
+        return view('superadmin.review-and-rating.index', compact('properties', 'most_visited_places'));
     }
 
     public function show($id) {
-
-
         $reviews = Review::with('user', 'properties')->where('property_id', $id)->get();
+
         return view('superadmin.review-and-rating.show', compact('reviews'));
     }
 }
--- resources/views/superadmin/review-and-rating/index.blade.php
+++ resources/views/superadmin/review-and-rating/index.blade.php
@@ -7,7 +7,12 @@
     <div class="grid grid-cols-6 gap-2">
         @foreach ($properties as $property )
         <a href="{{ route('review-and-rating.show', $property->id) }}"
-            class="col-span-6 lg:col-span-3 p-6 w-full rounded-lg border shadow-md bg-gray-800 border-gray-700">
+            class="col-span-6 relative lg:col-span-3 p-6 w-full rounded-lg border shadow-md bg-gray-800 border-gray-700">
+            @if ($loop->first)
+            <div class="text-white absolute top-4 right-4 bg-indigo-700 px-4 py-1 rounded">
+                Most Reviewed Place
+            </div>
+            @endif
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
                 style="fill: rgba(217, 203, 203, 1);transform: ;msFilter:;">
                 <path
@@ -20,18 +25,38 @@
             <h2 class="text-3xl text-white font-medium">
                 {{ $property->reviews->count() }} reviews
             </h2>
-            {{-- <a href="{{ route('businesses.properties', $business) }}"
-                class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                Read more
-                <svg aria-hidden="true" class="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </a> --}}
         </a>
         @endforeach
     </div>
+
+    <h2 class="text-md my-4 font-medium">Most Visited Places</h2>
+
+    <div class="grid grid-cols-6 gap-2">
+        @foreach ($most_visited_places as $most_visited_place )
+        <a href=""
+            class="relative col-span-6 lg:col-span-3 p-6 w-full rounded-lg border shadow-md bg-gray-800 border-gray-700">
+            @if ($loop->first)
+            <div class="text-white absolute top-4 right-4 bg-indigo-700 px-4 py-1 rounded">
+                Most Visited Place
+            </div>
+            @endif
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+                style="fill: rgba(217, 203, 203, 1);transform: ;msFilter:;">
+                <path
+                    d="M21 7h-6a1 1 0 0 0-1 1v3h-2V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zM8 6h2v2H8V6zM6 16H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V6h2v2zm4 8H8v-2h2v2zm0-4H8v-2h2v2zm9 4h-2v-2h2v2zm0-4h-2v-2h2v2z">
+                </path>
+            </svg>
+            <h5 class="mb-2 text-2xl font-semibold tracking-tight  text-white">
+                {{ $most_visited_place->property_name }}
+            </h5>
+            <h2 class="text-3xl text-white font-medium">
+                {{ $most_visited_place->total_number_of_adults + $most_visited_place->total_number_of_children +
+                $most_visited_place->total_number_of_infants + $most_visited_place->total_number_of_foreigner }}
+                Tourists
+            </h2>
+        </a>
+        @endforeach
+    </div>
+
 
 </x-app-sidebar-layout>
