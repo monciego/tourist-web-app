9df7661dfddafd2910a0be0a0127ba2d772ee28c
Jericho <jerichobantiquete@gmail.com>
2023-06-02 21:52:59 +0800

    feat: added contact form

--- resources/views/pages/homepage/menu-list/contact.blade.php
+++ resources/views/pages/homepage/menu-list/contact.blade.php
@@ -6,59 +6,177 @@
             <div class="mx-auto max-w-2xl lg:text-center">
                 <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Municipal Tourism Office</p>
             </div>
-            <img class="h-24 w-24 mx-auto mt-4" src="{{ asset('assets/images/logo.png') }}" alt="">
 
-            <div class="text-center my-4">
-                <h2>Experienced Dasol ... and bedazzled</h2>
-                <p>#garantisadongSerbisyoSaTurismo</p>
-            </div>
 
-            <div class="flex items-center justify-center">
-                <div class="inline ">
-                    <div class="text-base flex gap-4 mt-2  leading-7 text-gray-900">
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                            </svg>
+            <div class="max-w-7xl mx-auto px-4 py-10 sm:px-6 lg:px-8">
+                <div class="relative bg-white shadow-xl">
+                    <h2 class="sr-only">Contact us</h2>
+
+                    <div class="grid grid-cols-1 lg:grid-cols-3">
+                        <!-- Contact information -->
+                        <div class="relative overflow-hidden py-10 px-6 bg-indigo-700 sm:px-10 xl:p-12">
+                            <div class="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
+                                <svg class="absolute inset-0 w-full h-full" width="343" height="388"
+                                    viewBox="0 0 343 388" fill="none" preserveAspectRatio="xMidYMid slice"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M-99 461.107L608.107-246l707.103 707.107-707.103 707.103L-99 461.107z"
+                                        fill="url(#linear1)" fill-opacity=".1" />
+                                    <defs>
+                                        <linearGradient id="linear1" x1="254.553" y1="107.554" x2="961.66" y2="814.66"
+                                            gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#fff"></stop>
+                                            <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </div>
+                            <div class="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden"
+                                aria-hidden="true">
+                                <svg class="absolute inset-0 w-full h-full" width="359" height="339"
+                                    viewBox="0 0 359 339" fill="none" preserveAspectRatio="xMidYMid slice"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M-161 382.107L546.107-325l707.103 707.107-707.103 707.103L-161 382.107z"
+                                        fill="url(#linear2)" fill-opacity=".1" />
+                                    <defs>
+                                        <linearGradient id="linear2" x1="192.553" y1="28.553" x2="899.66" y2="735.66"
+                                            gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#fff"></stop>
+                                            <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </div>
+                            <div class="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block"
+                                aria-hidden="true">
+                                <svg class="absolute inset-0 w-full h-full" width="160" height="678"
+                                    viewBox="0 0 160 678" fill="none" preserveAspectRatio="xMidYMid slice"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M-161 679.107L546.107-28l707.103 707.107-707.103 707.103L-161 679.107z"
+                                        fill="url(#linear3)" fill-opacity=".1" />
+                                    <defs>
+                                        <linearGradient id="linear3" x1="192.553" y1="325.553" x2="899.66" y2="1032.66"
+                                            gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#fff"></stop>
+                                            <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </div>
+                            <h3 class="text-lg font-medium text-white">Contact information</h3>
+                            <div class="mt-6 text-base text-indigo-50 max-w-3xl">
+                                <h2 class="font-medium">Experienced Dasol ... and bedazzled</h2>
+                                <p>#garantisadongSerbisyoSaTurismo</p>
+                            </div>
+                            <dl class="mt-8 space-y-6">
+                                <dt><span class="sr-only">Phone number</span></dt>
+                                <dd class="flex text-base text-indigo-50">
+                                    <!-- Heroicon name: outline/phone -->
+                                    <svg class="flex-shrink-0 w-6 h-6 text-indigo-200"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                    </svg>
+                                    <div class="ml-3">
+                                        <a class="underline block" href="tel:(075) 636 9558">(075) 636
+                                            9558</a>
+                                        <a class="underline " href="tel:(075) 636 9558">(075) 636
+                                            9558</a>
+
+                                    </div>
+                                </dd>
+                                <dt><span class="sr-only">Email</span></dt>
+                                <dd class="flex text-base text-indigo-50">
+                                    <!-- Heroicon name: outline/mail -->
+                                    <svg class="flex-shrink-0 w-6 h-6 text-indigo-200"
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                    </svg>
+                                    <a class="underline  ml-3"
+                                        href="mailto:turismodasol@gmail.com">turismodasol@gmail.com</a>
+                                </dd>
+                            </dl>
+                            <ul role="list" class="mt-8 flex space-x-12">
+                                <li>
+                                    <a class="text-indigo-200 hover:text-indigo-100"
+                                        href="https://www.facebook.com/dasolpangasinan.gov.ph1" target="__blank">
+                                        <span class="sr-only">Facebook</span>
+                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" aria-hidden="true">
+                                            <path
+                                                d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1"
+                                                fill="currentColor" />
+                                        </svg>
+                                    </a>
+                                </li>
+
+
+                            </ul>
                         </div>
-                        <div>
-                            <a class="underline text-indigo-700 block" href="tel:(075) 636 9558">(075) 636 9558</a>
-                            <a class="underline text-indigo-700 " href="tel:(075) 636 9558">(075) 636 9558</a>
-                        </div>
-                    </div>
-                    <div class="text-base flex gap-4 mt-2 leading-7 text-gray-900">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                        </svg>
-                        <a class="underline text-indigo-700 "
-                            href="mailto:turismodasol@gmail.com">turismodasol@gmail.com</a>
 
-                    </div>
-                    <div class="text-base flex gap-4 mt-2 leading-7 text-gray-900">
-                        <a target="__blank" href="https://www.facebook.com/dasolbfp.pangasinan.9">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M2.00195 12.002C2.00312 16.9214 5.58036 21.1101 10.439 21.881V14.892H7.90195V12.002H10.442V9.80204C10.3284 8.75958 10.6845 7.72064 11.4136 6.96698C12.1427 6.21332 13.1693 5.82306 14.215 5.90204C14.9655 5.91417 15.7141 5.98101 16.455 6.10205V8.56104H15.191C14.7558 8.50405 14.3183 8.64777 14.0017 8.95171C13.6851 9.25566 13.5237 9.68693 13.563 10.124V12.002H16.334L15.891 14.893H13.563V21.881C18.8174 21.0506 22.502 16.2518 21.9475 10.9611C21.3929 5.67041 16.7932 1.73997 11.4808 2.01722C6.16831 2.29447 2.0028 6.68235 2.00195 12.002Z"
-                                    fill="#2E3A59"></path>
-                            </svg>
-                        </a>
-                        <a class="underline text-indigo-700 " href="https://www.facebook.com/dasolpangasinan.gov.ph1"
-                            target="__blank">Facebook</a>
+                        <!-- Contact form -->
+                        <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
+                            <h3 class="text-lg font-medium text-gray-900">Send us a message</h3>
+                            <form action="https://formspree.io/f/xyyaqzbr" method="POST"
+                                class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                                <div>
+                                    <label for="first-name" class="block text-sm font-medium text-gray-900">First
+                                        name</label>
+                                    <div class="mt-1">
+                                        <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                                            class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                    </div>
+                                </div>
+                                <div>
+                                    <label for="last-name" class="block text-sm font-medium text-gray-900">Last
+                                        name</label>
+                                    <div class="mt-1">
+                                        <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                                            class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                    </div>
+                                </div>
+                                <div>
+                                    <label for="email" class="block text-sm font-medium text-gray-900">Email</label>
+                                    <div class="mt-1">
+                                        <input id="email" name="email" type="email" autocomplete="email"
+                                            class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="flex justify-between">
+                                        <label for="phone" class="block text-sm font-medium text-gray-900">Phone</label>
+                                        <span id="phone-optional" class="text-sm text-gray-500">Optional</span>
+                                    </div>
+                                    <div class="mt-1">
+                                        <input type="text" name="phone" id="phone" autocomplete="tel"
+                                            class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+                                            aria-describedby="phone-optional">
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <div class="flex justify-between">
+                                        <label for="message"
+                                            class="block text-sm font-medium text-gray-900">Message</label>
+                                        <span id="message-max" class="text-sm text-gray-500">Max. 500
+                                            characters</span>
+                                    </div>
+                                    <div class="mt-1">
+                                        <textarea id="message" name="message" rows="4"
+                                            class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
+                                            aria-describedby="message-max"></textarea>
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2 sm:flex sm:justify-end">
+                                    <button type="submit"
+                                        class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto">Submit</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-
-            <div class="flex items-center justify-center">
-                <img src="{{ asset('assets/images/about/discover-explore-love.png') }}" alt=""
-                    class="h-[20rem] w-[30rem] mt-4">
-            </div>
-
-
         </div>
     </div>
 </x-app-layout>
