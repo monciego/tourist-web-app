84511c8bd86eddbdcaa3b82a4b86229f2db0eeba
Jericho <jerichobantiquete@gmail.com>
2023-06-15 11:07:00 +0800

    feat: responsive table

--- resources/views/staff/verified-tickets/table.blade.php
+++ resources/views/staff/verified-tickets/table.blade.php
@@ -1,74 +1,74 @@
-{{-- Table --}}
 @unless ($tickets->isEmpty())
-<div class="overflow-x-auto">
-    <table class="table-auto w-full">
-        {{-- table header --}}
-        <thead class="text-xs uppercase text-slate-400 bg-slate-50 rounded-sm">
-            <tr>
-                <th class="p-2">
-                    <div class="font-semibold text-left">Ticket Number</div>
-                </th>
-                <th class="p-2">
-                    <div class="font-semibold text-left">Verified by</div>
-                </th>
-                <th class="p-2">
-                    <div class="font-semibold text-left">Contact Person</div>
-                </th>
-                <th class="p-2">
-                    <div class="font-semibold text-left">Date of Tour</div>
-                </th>
-                <th class="p-2">
-                    <div class="font-semibold text-left">Status</div>
-                </th>
-                <th class="p-2">
-                    <div class="font-semibold text-left">Action</div>
-                </th>
-            </tr>
-        </thead>
-        {{-- table body --}}
-        <tbody class="text-sm font-medium divide-y divide-slate-100">
-            {{-- row --}}
-            @foreach ($tickets as $ticket)
-            <tr>
-                <td class="p-2">
-                    <div class="text-slate-800 capitalize">
-                        {{ $ticket->tour_code }}
-                    </div>
-                </td>
-                <td class="p-2 ">
-                    <div>
-                        {{ $ticket->verified_by }}
-                    </div>
-                </td>
-                <td class="p-2 ">
-                    <div>
-                        {{ $ticket->tour_contact_person }}
-                    </div>
-                </td>
+<div>
+    <div class="flex flex-col">
+        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-300">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                    Ticket Number
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Verified by
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Contact Person
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Date of Tour
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Status
+                                </th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                    Action
+                                </th>
 
-                <td class="p-2 ">
-                    {{ \Carbon\Carbon::parse($ticket->tour_date)->isoFormat('MMM D YYYY')}}
-                </td>
-                <td class="p-2 ">
-                    @if ($ticket->status === 'still_in_the_area' || $ticket->status === null)
-                    <span class="bg-green-600 text-white px-4 py-2 rounded">
-                        Still in the area
-                    </span>
-                    @elseif ($ticket->status === 'already_left')
-                    <span class="bg-red-600 text-white px-4 py-2 rounded">
-                        Already Left
-                    </span>
-                    @endif
-                </td>
-                <td class="p-2 ">
-                    <a href="{{ route('verified.tickets.details', $ticket) }}"
-                        class="hover:underline text-indigo-700">More
-                        Details</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 bg-white">
+                            @foreach ($tickets as $ticket)
+                            <tr>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                    {{ $ticket->tour_code }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    {{ $ticket->verified_by }}
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    {{ $ticket->tour_contact_person }}
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    {{ \Carbon\Carbon::parse($ticket->tour_date)->isoFormat('MMM D YYYY')}}
+                                </td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    @if ($ticket->status === 'still_in_the_area' || $ticket->status ===
+                                    null)
+                                    <span class="bg-green-600 text-white px-4 py-2 rounded">
+                                        Still in the area
+                                    </span>
+                                    @elseif ($ticket->status === 'already_left')
+                                    <span class="bg-red-600 text-white px-4 py-2 rounded">
+                                        Already Left
+                                    </span>
+                                    @endif
+                                </td>
+
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    <a href="{{ route('verified.tickets.details', $ticket) }}"
+                                        class="hover:underline text-indigo-700">More
+                                        Details</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @else
 <div class="flex-col flex items-center justify-center">
